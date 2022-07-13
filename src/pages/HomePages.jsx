d95517e145b38ad1754d35853ae8bbdd4f43c453
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-13 09:05:30 +0700

    keos

---
+++ src/pages/HomePages.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+export class HomePage extends Component {
+    render() {
+        return (
+        <div>
+            <Header />
+        </div>
+        );
+    }
+}
+
+export default HomePage;
